5fafd2c51f09a87d32b06a5fee0a6b94bf20b3b7
Andy Edwards <andyhedwards@gmail.com>
2015-12-11 14:54:12 +1300

    Refactor so marker is removed then added

--- app/main.js
+++ app/main.js
@@ -6,12 +6,10 @@ import 'fetch';
 import {issLocation} from './api'
 
 
-setInterval(function(){
   issLocation().then(function(res){
     console.log(res)
     ReactDOM.render(<Map location={res} />, document.getElementById('app'))
   })
-}, 2000)
 
 
 
